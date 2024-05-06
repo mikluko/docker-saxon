5ba85fcc1d33e5f6b6ff5e25d6944969af432f18
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2024-05-06 16:10:30 +0200

    Update Docker command in example.sh script

--- example/example.sh
+++ example/example.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 (
   cd "$( dirname "${BASH_SOURCE[0]}" )"
-  docker run --rm -v $(pwd):/mnt mikluko/saxon-he transform input.xml transform.xsl
+  docker run --rm -v "$(pwd):/data" -w /data mikluko/saxon-he:latest transform input.xml transform.xsl
 )
